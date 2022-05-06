62e97998daa82cb775189fc2e0de2aac7193e601
Kyle Horne <kylehorne@outlook.com>
2022-05-06 13:45:16 -0700

    Route Setup Complete - Full CRUD on Backend

--- app.js
+++ app.js
@@ -18,7 +18,8 @@ app.use(cors());
 app.use(express.json());
 
 //requests and responses
-app.get('/posts', function(req, res){
+//All Posts
+app.get('/api/posts', function(req, res){
   dbConnection
   .select('*')
   .from('posts')
@@ -29,6 +30,109 @@ app.get('/posts', function(req, res){
           'The data you are looking for could not be found. Please try again'
     }))
 });
+
+//All Posts by User
+app.get('/api/posts/user/:user', function(req, res){
+  dbConnection
+  .select('*')
+  .where({created_by: req.params.user})
+  .from('posts')
+  .then(data => res.status(200).json(data))
+  .catch(err =>
+    res.status(404).json({
+        message:
+          'The data you are looking for could not be found. Please try again'
+    }))
+});
+
+
+//Create Post
+app.post('/api/posts', function(req, res){
+  dbConnection
+  .insert({ title: req.body.title, content: req.body.content, created_by: req.body.created_by }).from('posts')
+      .then((data) => res.status(201).json(data))
+      .catch((err) => {
+        res.status(404).json({ message: "Something is wrong."})
+    })
+  });
+
+
+//Delete Post
+app.delete('/api/posts/', function(req, res){
+dbConnection
+.delete('*').where({title: req.body.title, created_by:req.body.user}).from('posts')
+    .then((data) => res.status(201).json(data))
+    .catch((err) => {
+      res.status(404).json({ message: "Something is wrong."})
+  })
+});
+
+
+//Search Post
+app.get('/api/posts/specific/:title', function(req, res){
+dbConnection
+.select('*')
+.where({title: req.params.title})
+.from('posts')
+.then((data) => res.status(201).json(data))
+.catch((err) => {
+      res.status(404).json({ message: "Something is wrong."})
+  })
+});
+
+
+//Edit Post
+app.patch('/api/posts/edit', function(req, res){
+  dbConnection
+  .update({content: req.body.content})
+  .where({title: req.body.title, created_by:req.body.user})
+  .from('posts')
+  .then((data) => res.status(201).json(data))
+  .catch((err) => {
+        res.status(404).json({ message: "Something is wrong."})
+    })
+  });
+
+
+//Register
+app.post('/register', async (req, res) => {
+  try {
+    const {username, password, first_name, last_name} = req.body;
+    const user = req.body;
+    const hash = await bcrypt.hash(password, 12);
+    await dbConnection.insert({username: username, password_hash: hash, first_name: first_name, last_name: last_name}).from('users');
+    res.status(201).json(
+      "success"
+     );
+  } catch(e) {
+    res.status(500).send('Something Went Wrong');
+  }
+});
+
+//Login
+app.post('/login', async(req, res) => {
+  try {
+    const {username, password} = req.body;
+    user = await dbConnection.select('*').where({username: username}).from('users')
+    if (user.length){
+      const validPass = await bcrypt.compare(password, user[0].password_hash);
+      if(validPass) {
+        res.status(201).json({
+          user: username,
+          token:generateToken(username)
+         });
+      }
+      else {
+        res.json('Wrong Password')
+      }
+      }
+      else {
+        res.status(404).json('User not found')
+      }
+    } catch(e) {
+    res.status(500).send('Something Went Wrong');
+    }
+  });
 
 
 module.exports = app;
--- seeds/seed_posts.js
+++ seeds/seed_posts.js
@@ -5,9 +5,10 @@
  exports.seed = async function(knex) {
   await knex('posts').del()
   await knex('posts').insert([
-    {title: 'First Post', content: 'This blog was the first full stack app I deployed!', created_by: 'Kyle'},
+    {title: 'First Post', content: 'This blog is my best project yet!', created_by: 'Kyle'},
     {title: 'Huh?!', content: 'Posts seeded by Knex!', created_by: 'Kyle'},
     {title: 'Registering an Account', content: 'If you register, you can make, edit, and delete posts. However, your posts will disappear when the server restarts.', created_by: 'Kyle'},
-    {title: 'Project', content: 'Full Stack Full Crud', created_by: 'Kyle'}
+    {title: 'Project', content: 'Full Stack Full Crud', created_by: 'Kyle'},
+    {title: 'Go Kyle', content: 'I knew you could do it!', created_by: 'Caitlin'}
   ]);
 };
